48c144c49f5399ad7a972a1f9d607034c3411e10
aurelia ritter <aureliaritter@gmail.com>
2023-05-07 21:48:10 +0200

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurant1 = { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                phone_number: Faker::Number.leading_zero_number(digits: 10), category: 'italian' }
+restaurant2 = { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                phone_number: Faker::Number.leading_zero_number(digits: 10), category: 'japanese' }
+restaurant3 = { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                phone_number: Faker::Number.leading_zero_number(digits: 10), category: 'french' }
+restaurant4 = { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                phone_number: Faker::Number.leading_zero_number(digits: 10), category: 'belgian' }
+restaurant5 = { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                phone_number: Faker::Number.leading_zero_number(digits: 10), category: 'chinese' }
+
+[restaurant1, restaurant2, restaurant3, restaurant4, restaurant5].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
